0a89356f8cb56b84208ad8c3682d06f7e6d3e0e7
juanprietob <juanprietob@gmail.com>
2016-07-12 15:03:08 -0400

    ENH: Read images and create pickle files using python and nrrd format

--- py/readImages.py
+++ py/readImages.py
@@ -1,40 +1,132 @@
 import glob, os
-from optparse import OptionParser
 import numpy as np
 import nrrd
+import argparse
+from six.moves import cPickle as pickle
 
-parser = OptionParser()
-parser.add_option("-d", "--dir", dest="directory",
-                  help="Input directory", metavar="FILE")
 
-(options, args) = parser.parse_args()
-print options
+parser = argparse.ArgumentParser()
+parser.add_argument('--dir', help='Input directory', required=True)
+#parser.add_argument("--force", help="Force pickle file generation")
+parser.add_argument("--force", help="Force pickle file generation")
+parser.add_argument('-s','--imgsize', nargs=4, help='Set the image size, [numberOfComponents, sizex, sizey, sizez]', default=[2, 19, 19, 3], type=int)
+parser.add_argument("--trainSize", help="Output training size dataset", default=200000, type=int)
+parser.add_argument("--validSize", help="Output validation size dataset", default=10000, type=int)
+parser.add_argument("--testSize", help="Output test size dataset", default=10000, type=int)
 
-image_files = glob.glob(os.path.join(options.directory, '*.nrrd'))
+args = parser.parse_args()
 
-if len(image_files) > 0:
+imgsize = args.imgsize;
+rootdir = args.dir
+force = args.force
+train_size = args.trainSize
+valid_size = args.validSize
+test_size = args.testSize
+img_head = {}
 
-	image_data, options = nrrd.read(image_files[0])
-	imgsize = options["sizes"];
+dirs = [d for d in os.listdir(rootdir) if os.path.isdir(os.path.join(rootdir, d))]
 
-	dataset = np.ndarray(shape=(len(image_files), imgsize[0], imgsize[1], imgsize[2], imgsize[3]), dtype=np.float32)
-	image_index = 0
+def maybe_pickle(dirs, force):
+	dataset_names = []
 
-	print options
+	for d in dirs:
 
-	for file in image_files:
-		print(file)
+		set_filename = os.path.join(rootdir, d + '.pickle')
+		if os.path.exists(set_filename) and not force:
+			# You may override by setting force=True.
+			print('%s already present - Skipping pickling.' % set_filename)
+		else:
+			print('Pickling %s.' % set_filename)
 
-		try:
-			
-			image_data, options = nrrd.read(file)
+			image_files = glob.glob(os.path.join(rootdir, d, '*.nrrd'))
 
-		  	dataset[image_index, :, :] = image_data
+			dataset = np.ndarray(shape=(len(image_files), imgsize[0], imgsize[1], imgsize[2], imgsize[3]), dtype=np.float32)
+			num_images = 0
 
-		  	image_index += 1
+			for file in image_files:
+				print(file)
 
-		except IOError as e:
-			print('Could not read:', file, '- it\'s ok, skipping.')
+				try:
+					
+					image_data, img_head = nrrd.read(file)
 
-	num_images = image_index
-	dataset = dataset[0:num_images, :, :]
+					dataset[num_images, :, :, :] = image_data
+					num_images += 1
+
+				except IOError as e:
+					print('Could not read:', file, '- it\'s ok, skipping.')
+
+			dataset = dataset[0:num_images, :, :]
+
+			try:
+				with open(set_filename, 'wb') as f:
+					pickle.dump(dataset, f, pickle.HIGHEST_PROTOCOL)
+			except Exception as e:
+				print('Unable to save data to', set_filename, ':', e)
+
+		dataset_names.append(set_filename)
+
+	return dataset_names
+
+train_datasets = maybe_pickle(dirs, force)
+
+
+def make_arrays(nb_rows, imgsize):
+  if nb_rows:
+    dataset = np.ndarray((nb_rows, imgsize[0], imgsize[1], imgsize[2], imgsize[3]), dtype=np.float32)
+    labels = np.ndarray(nb_rows, dtype=np.int32)
+  else:
+    dataset, labels = None, None
+  return dataset, labels
+
+def merge_datasets(pickle_files, train_size, valid_size=0):
+  num_classes = len(pickle_files)
+  valid_dataset, valid_labels = make_arrays(valid_size, imgsize)
+  train_dataset, train_labels = make_arrays(train_size, imgsize)
+  vsize_per_class = valid_size // num_classes
+  tsize_per_class = train_size // num_classes
+    
+  start_v, start_t = 0, 0
+  end_v, end_t = vsize_per_class, tsize_per_class
+  end_l = vsize_per_class+tsize_per_class
+  for label, pickle_file in enumerate(pickle_files):       
+    try:
+      with open(pickle_file, 'rb') as f:
+        letter_set = pickle.load(f)
+        # let's shuffle the letters to have random validation and training set
+        np.random.shuffle(letter_set)
+        if valid_dataset is not None:
+          valid_letter = letter_set[:vsize_per_class, :, :, :]
+          valid_dataset[start_v:end_v, :, :] = valid_letter
+          valid_labels[start_v:end_v] = label
+          start_v += vsize_per_class
+          end_v += vsize_per_class
+                    
+        train_letter = letter_set[vsize_per_class:end_l, :, :]
+        train_dataset[start_t:end_t, :, :] = train_letter
+        train_labels[start_t:end_t] = label
+        start_t += tsize_per_class
+        end_t += tsize_per_class
+    except Exception as e:
+      print('Unable to process data from', pickle_file, ':', e)
+      raise
+    
+  return valid_dataset, valid_labels, train_dataset, train_labels
+            
+           
+
+valid_dataset, valid_labels, train_dataset, train_labels = merge_datasets(train_datasets, train_size, valid_size)
+#_, _, test_dataset, test_labels = merge_datasets(test_datasets, test_size)
+
+def randomize(dataset, labels):
+  permutation = np.random.permutation(labels.shape[0])
+  shuffled_dataset = dataset[permutation,:,:,:]
+  shuffled_labels = labels[permutation]
+  return shuffled_dataset, shuffled_labels
+
+train_dataset, train_labels = randomize(train_dataset, train_labels)
+#test_dataset, test_labels = randomize(test_dataset, test_labels)
+valid_dataset, valid_labels = randomize(valid_dataset, valid_labels)
+
+
+nrrd.write("out.nrrd", train_dataset[10], img_head)

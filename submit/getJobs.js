f489bbeac589eeb18c4d3b72f7616a45bc6d63ce
juanprietob <juanprietob@gmail.com>
2016-07-14 10:34:19 -0400

    ENH: Add option to check the status when getting the jobs

--- submit/getJobs.js
+++ submit/getJobs.js
@@ -1,6 +1,7 @@
 var clusterpost = require("clusterpost-lib");
 var path = require('path');
 var argv = require('minimist')(process.argv.slice(2));
+var Promise = require('bluebird');
 
 
 const getConfigFile = function (env, base_directory) {
@@ -16,11 +17,13 @@ const getConfigFile = function (env, base_directory) {
 
 if(!argv["dir"]){
     console.error("Help: ");
-    console.error("--dir  Input directory must contain files PD.nii.gz, T2.nii.gz and pvec.nii.gz");
+    console.error("--dir  Output directory");
     process.exit(1);
 }
 
 var outputdir = argv["dir"];
+var status = argv["status"];
+var jobid = argv["j"];
 
 var env = process.env.NODE_ENV;
 if(!env) throw "Please set NODE_ENV variable.";
@@ -37,7 +40,41 @@ clusterpost.setAgentOptions(agentoptions);
 
 clusterpost.userLogin(conf.user)
 .then(function(res){
-    return clusterpost.getJobs("extractMSLesion");
+    if(!status){
+        if(!jobid){
+            return clusterpost.getJobs("extractMSLesion", "DONE")
+            .then(function(jobs){
+                return Promise.map(jobs, function(job){
+                    return clusterpost.getJobOutputs(job, path.join(outputdir, job._id));
+                })
+                .then(function(res){
+                    return Promise.map(jobs, function(job){
+                        return clusterpost.deleteJob(job._id);
+                    });
+                });
+            });
+        }else{
+            return clusterpost.getDocument(jobid)
+            .then(function(job){
+                return clusterpost.getJobOutputs(job, path.join(outputdir, jobid))
+            })
+            .then(function(){
+                return clusterpost.deleteJob(jobid);
+            });
+        }
+    }else{
+        if(!jobid){
+            return clusterpost.getJobs("extractMSLesion", "RUN")
+            .then(function(jobs){
+                return Promise.map(jobs, function(job){
+                    return clusterpost.updateJobStatus(job._id);
+                });
+            });
+        }else{
+            return clusterpost.updateJobStatus(jobid);
+        }
+    }
+    
 })
 .then(function(res){
     console.log(res);
